8daba1a462e7e9457dcf42a1989872a3a295fa80
Simon Levy <simon.levy@onthebeach.co.uk>
2018-10-03 10:22:34 +0100

    triangle errors

--- the-language/lib/triangle.rb
+++ the-language/lib/triangle.rb
@@ -13,8 +13,19 @@
 # and
 #   about_triangle_project_2.rb
 #
+def calculate_if_longest_side_biggar_than_sum_of_other_sides?(sides)
+  longest_side = sides.max
+  sides.delete_at(sides.index(longest_side))
+  sides[0] + sides[1] <= longest_side
+end
+
+
 def triangle(a, b, c)
-  if a == b && b == c
+  if a < 1 || b < 1 || c < 1
+    raise TriangleError.new("Plebs")
+  elsif calculate_if_longest_side_biggar_than_sum_of_other_sides?([a, b, c])
+    raise TriangleError.new("More plebs")
+  elsif a == b && b == c
     :equilateral
   elsif a == b || b == c || a == c
     :isosceles
