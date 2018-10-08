aa411856232e1ef7fb08c39d64b0b718de8cf0be
Steve Tooke <tooky@cucumber.io>
2018-10-08 12:02:09 +0100

    Shameless green

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,2 +1,34 @@
 class Bottles
+  def sing
+    verses(99,0)
+  end
+
+  def verses(start, finish)
+    start.downto(finish).map { |number| verse(number) }.join("\n")
+  end
+
+  def verse(number)
+    case number
+    when 0
+      <<~VERSE
+        No more bottles of beer on the wall, no more bottles of beer.
+        Go to the store and buy some more, 99 bottles of beer on the wall.
+        VERSE
+    when 1
+      <<~VERSE
+        1 bottle of beer on the wall, 1 bottle of beer.
+        Take it down and pass it around, no more bottles of beer on the wall.
+        VERSE
+    when 2
+      <<~VERSE
+        2 bottles of beer on the wall, 2 bottles of beer.
+        Take one down and pass it around, 1 bottle of beer on the wall.
+        VERSE
+    else
+      <<~VERSE
+        #{number} bottles of beer on the wall, #{number} bottles of beer.
+        Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+        VERSE
+    end
+  end
 end
