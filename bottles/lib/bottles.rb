1acf59871a15acda277d79ba9ba8f8d5a1997f99
Steve Tooke <tooky@cucumber.io>
2018-10-09 07:09:45 +0100

    Extract class

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -8,49 +8,60 @@ class Bottles
   end
 
   def verse(number)
+    bottle_number = BottleNumber.new(number)
+    next_bottle_number = BottleNumber.new(bottle_number.successor)
     <<~VERSE
-      #{ quantity(number).capitalize } #{ container(number) } of beer on the wall, #{ quantity(number) } #{ container(number) } of beer.
-      #{ action(number) }, #{ quantity(successor(number)) } #{ container(successor(number)) } of beer on the wall.
+      #{ bottle_number.quantity.capitalize } #{ bottle_number.container } of beer on the wall, #{ bottle_number.quantity } #{ bottle_number.container } of beer.
+      #{ bottle_number.action }, #{ next_bottle_number.quantity } #{ next_bottle_number.container } of beer on the wall.
       VERSE
   end
 
-  def container(number)
-    if number == 1
-      "bottle"
-    else
-      "bottles"
-    end
-  end
+  class BottleNumber
+    attr_reader :number
 
-  def pronoun(number)
-    if number == 1
-      "it"
-    else
-      "one"
+    def initialize(number)
+      @number = number
     end
-  end
 
-  def quantity(number)
-    if number == 0
-      "no more"
-    else
-      number.to_s
+    def container
+      if number == 1
+        "bottle"
+      else
+        "bottles"
+      end
     end
-  end
 
-  def action(number)
-    if number == 0
-      "Go to the store and buy some more"
-    else
-      "Take #{ pronoun(number) } down and pass it around"
+    def pronoun
+      if number == 1
+        "it"
+      else
+        "one"
+      end
     end
-  end
 
-  def successor(number)
-    if number == 0
-      99
-    else
-      number - 1
+    def quantity
+      if number == 0
+        "no more"
+      else
+        number.to_s
+      end
+    end
+
+    def action
+      if number == 0
+        "Go to the store and buy some more"
+      else
+        "Take #{ pronoun } down and pass it around"
+      end
+    end
+
+    def successor
+      if number == 0
+        99
+      else
+        number - 1
+      end
     end
   end
 end
+
