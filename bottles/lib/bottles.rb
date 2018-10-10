f3873c42fdb01b1bcf60c7d266edb19e2c98eb89
Si Levy <simonjlevy@gmail.com>
2018-10-10 14:55:22 +0100

    refactoring if statements into polymorphicness, new 6 pack requirement

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -8,8 +8,8 @@ class Bottles
   end
 
   def verse(number)
-    bottle_number = BottleNumber.new(number)
-    next_bottle_number = BottleNumber.new(bottle_number.successor)
+    bottle_number = BottleNumber.for(number)
+    next_bottle_number = bottle_number.successor
     <<~VERSE
       #{ bottle_number.to_s.capitalize } of beer on the wall, #{ bottle_number } of beer.
       #{ bottle_number.action }, #{ next_bottle_number } of beer on the wall.
@@ -19,52 +19,80 @@ class Bottles
   class BottleNumber
     attr_reader :number
 
+    def self.for(number)
+      case number
+      when 6
+        BottleNumberSix.new(number)
+      when 0
+        BottleNumberZero.new(number)
+      when 1
+        BottleNumberOne.new(number)
+      else
+        BottleNumber.new(number)
+      end
+    end
+
     def initialize(number)
       @number = number
     end
 
     def container
-      if number == 1
-        "bottle"
-      else
-        "bottles"
-      end
+      "bottles"
     end
 
     def pronoun
-      if number == 1
-        "it"
-      else
-        "one"
-      end
+      "one"
     end
 
     def quantity
-      if number == 0
-        "no more"
-      else
-        number.to_s
-      end
+      number.to_s
     end
 
     def action
-      if number == 0
-        "Go to the store and buy some more"
-      else
-        "Take #{ pronoun } down and pass it around"
-      end
+      "Take #{ pronoun } down and pass it around"
     end
 
     def successor
-      if number == 0
-        99
-      else
-        number - 1
-      end
+      BottleNumber.for(number - 1)
     end
-    
+
     def to_s
       "#{quantity} #{container}"
+    end
+
+  end
+
+  class BottleNumberZero < BottleNumber
+    def quantity
+      "no more"
+    end
+
+    def action
+      "Go to the store and buy some more"
+    end
+
+    def successor
+      BottleNumber.for(99)
+    end
+  end
+
+  class BottleNumberOne < BottleNumber
+    def container
+      "bottle"
+    end
+
+    def pronoun
+      "it"
+    end
+  end
+
+  class BottleNumberSix < BottleNumber
+    def container
+      "six pack"
+    end
+
+    def quantity
+      "1"
     end
   end
 end
